4bc8efce883a174f4ce9e4aae018f8c9f3de447e
Javul Mulla <javulmulla8@gmail.com>
2022-08-09 21:49:11 +0530

    Javul: implemented 3 player options-Ladder,Snake and Noplay

--- src/snakeandladdder/SnakeNLadder.java
+++ src/snakeandladdder/SnakeNLadder.java
@@ -3,12 +3,34 @@ import java.util.Random;
 
 public class SnakeNLadder {
 	static int POSITION = 0;
+	static final int IS_LADDER = 1;
+	static final int IS_SNAKE = 2;
+	static final int NO_PLAY = 0;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake And ladder");
 		System.out.println("Current Position: " + POSITION);
 		int dieNumber = dieRoll();
+		start(dieNumber);
 		System.out.println("Die Number is : " + dieNumber);
+		System.out.println("Current Position: " + POSITION);
+	}
+
+	private static void start(int dieNumber) {
+		int option = (int) (Math.random() * 10) % 3;
+		System.out.println(option);
+		switch (option) {
+		case IS_LADDER:
+			System.out.println("You got ladder");
+			POSITION += dieNumber;
+			break;
+		case IS_SNAKE:
+			System.out.println("you got snake");
+			POSITION -= dieNumber;
+			break;
+		case NO_PLAY:
+			System.out.println("No Play");
+		}
 	}
 
 	private static int dieRoll() {
