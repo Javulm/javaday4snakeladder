0955c22db783b83f8b66cf6905a070f0d5c2c577
Javul Mulla <javulmulla8@gmail.com>
2022-08-09 21:17:18 +0530

    Javul: Snake and ladder  welcome message

---
+++ src/snakeandladdder/SnakeNLadder.java
@@ -0,0 +1,10 @@
+package snakeandladdder;
+
+public class SnakeNLadder {
+	static int POSITION = 0;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake And ladder");
+		System.out.println("Current Position : " + POSITION);
+	}
+}
