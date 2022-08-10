01a0857241f425bfa209a89497885bc52e8920bf
Javul Mulla <javulmulla8@gmail.com>
2022-08-10 11:35:15 +0530

    Javul: Implemented two players function in snake and ladder game

--- src/snakeandladdder/SnakeNLadder.java
+++ src/snakeandladdder/SnakeNLadder.java
@@ -2,46 +2,79 @@ package snakeandladdder;
 import java.util.Random;
 
 public class SnakeNLadder {
-	static int POSITION = 0;
+	static int CHANCE=1;
 	static final int IS_LADDER = 1;
 	static final int IS_SNAKE = 2;
 	static final int NO_PLAY = 0;
-	static int TOTAL_DIE_ROLL=0;
+	
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake And ladder");
-		System.out.println("Current Position: " + POSITION);
-		 while (POSITION!=100) {
-		int dieNumber = dieRoll();
-		start(dieNumber);
-		System.out.println("Die Number is : " + dieNumber);
-		System.out.println("Current Position: " + POSITION);
-	}
-		 System.out.println("Number of times die rolled : "+TOTAL_DIE_ROLL);
-	}
-	private static void start(int dieNumber) {
+		player person1 = new player();
+		player person2 = new player();
+		System.out.println("Current Position of person1: " + person1.POSITION);
+		System.out.println("Current Position of person2: " + person2.POSITION);
+		while (person1.POSITION!=100 && person2.POSITION!=100) {
+			int dieNumber = dieRoll();
+			System.out.println("Die Number is : " + dieNumber);
+			if(CHANCE==1) {
+				person1.TOTAL_DIE_ROLL++;
+				start(dieNumber, person1);
+			}
+			else {
+				person1.TOTAL_DIE_ROLL++;
+				start(dieNumber, person2);
+			}
+		}
+			if (person1.POSITION==100) {
+				System.out.println("The winner is person1");
+				System.out.println("The total die played by person1 : "+person1.TOTAL_DIE_ROLL);
+			}
+			else if (person2.POSITION==100) {
+				System.out.println("The winner is person2");
+				System.out.println("The total die played by person2 : "+person2.TOTAL_DIE_ROLL);
+			}
+		}
+	    private static void start(int dieNumber, player person) {
 		int option = (int) (Math.random() * 10) % 3;
 		switch (option) {
 		case IS_LADDER:
-			System.out.println("You got ladder");
-			if (POSITION+dieNumber<=100)
-			POSITION += dieNumber;
+			System.out.println("person" +CHANCE+ " got ladder");
+			if (player.POSITION + dieNumber<=100)
+				player.POSITION += dieNumber;
+				System.out.println("current position: " + player.POSITION);
 			break;
 		case IS_SNAKE:
-			System.out.println("you got snake");
-			POSITION -= dieNumber;
-			if (POSITION<0)
-                POSITION =0;
+			System.out.println("person" +CHANCE+ " got snake");
+			player.POSITION -= dieNumber;
+			if (player.POSITION<0) {
+                player.POSITION =0;
+			System.out.println("current position: " + player.POSITION);
+			}
+			if(CHANCE==1)
+				CHANCE=2;
+			else
+				CHANCE=1;
 			break;
 		case NO_PLAY:
 			System.out.println("No Play");
+			if(CHANCE==1)
+				CHANCE=2;
+			else
+				CHANCE=1;
 		}
 	}
 
 	private static int dieRoll() {
 		Random random = new Random();
 		int dieNumber = random.nextInt(6) + 1;
-		 TOTAL_DIE_ROLL++;
+		 player.TOTAL_DIE_ROLL++;
 		return dieNumber;
 	}
 }
+
+class player{
+	static int POSITION = 0;
+	static int TOTAL_DIE_ROLL=0;
+}
+
